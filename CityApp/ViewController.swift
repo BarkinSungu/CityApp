133e217b9120a964d8eac0afe1aff04a13289300
BarkinSungu <barkinsungu@gmail.com>
2022-03-01 20:01:07 +0300

    Initial Commit

---
+++ CityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityApp
+//
+//  Created by Barkın Süngü on 1.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
